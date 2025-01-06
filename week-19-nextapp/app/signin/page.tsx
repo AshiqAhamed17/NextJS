146834e95d6bccfe192962d7ad2a7cc84915ce1f
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-06 21:12:04 +0530

    page.tsx

---
+++ week-19-nextapp/app/signin/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        
+        <div>
+            <h1> SignIn </h1>
+            <input type="text" placeholder="username" />
+            <button>Sign in</button>
+        </div>
+    )
+}
